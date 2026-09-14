287cec42b217acc2e88df7a46868ec0cd1157b89
t1mch1k12 <kim228455@gmail.com>
2026-09-14 15:02:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-"""
-    --- description ---
-    - [x] создать репозиторий проекта
-    - [x] реализовать цикл приложения
-    - [] реализовать хранилище задач
-"""
+import os
+import sys
+import platform
 from unittest import case
 
 is_running = True
-collection = []
+collection = [ "task1", "task2", "task3", "task4" ]
+
+def system_info():
+    print("\n--- Характеристики системы ---")
+    print("Операционная система:", platform.system())
+    print("Версия ОС:", platform.version())
+    print("Архитектура:", platform.architecture()[0])
+    print("Процессор:", platform.processor())
+    print("Версия Python:", platform.python_version())
 
 print("Добро пожаловать!")
 while is_running:
@@ -15,19 +20,22 @@ while is_running:
           "2- добавить задачу |\n"
           "3- редактирование задачи |\n"
           "4- удаление задачи |\n"
-          "5- выход |\n")
+          "5- характеристики ОС |\n"
+          "6- выход |\n")
+
     choice_user = input("Введите свой выбор")
-    match choice_user:
+    match str(choice_user):
         case "1":
-            for item in enumerate(collection):
+            for key, item in enumerate(collection):
                 print( key + 1,  item)
         case "2":
             name_task = input("Введите имя задачи")
             collection.append(name_task)
         case '3':
-            for item in enumerate(collection):
+            for key, item in enumerate(collection):
                 print(key + 1, item)
             select_edit = int(input("Введите номер задачи для редактирования"))
+            edit_name = input("Введите новое название задачи: ")
             collection[select_edit - 1] = edit_name
         case '4':
             for item in enumerate(collection):
@@ -35,8 +43,9 @@ while is_running:
             delete_edit = int(input("Введите номер задачи для редактирования"))
             collection.pop(delete_edit - 1)
         case "5":
+            system_info()
+        case "6":
             is_running = False
             print("До свидания!")
         case _:
             print("Такого тут нет!")
-
