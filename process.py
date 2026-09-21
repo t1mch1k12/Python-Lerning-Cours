5566bfa7f60d3791e4ec537f4d593529b5746c66
t1mch1k12 <kim228455@gmail.com>
2026-09-21 15:22:17 +0300

    Create process.py

---
+++ process.py
@@ -0,0 +1,32 @@
+﻿"""
+скрипт для демонстрации процессов
+"""
+import os
+import subprocess
+import multiprocessing as mp
+import time
+def _start():
+    print('start')
+    #print(f"{os.getpid()}")
+    #print(f"{os.getppid()}")
+
+    process = mp.Process(target=welcome, args=("welcome",))
+    process.start()
+    process.join()
+    print(process.name)
+    print(process.is_alive())
+
+
+def welcome(message):
+    print(message)
+    time.sleep(5)
+    work()
+
+def work():
+    print('work')
+    finish()
+
+def finish():
+    print('finish')
+
+_start()
